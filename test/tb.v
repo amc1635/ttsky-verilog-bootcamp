d499b184948852a4791dd2e7889ebbbca9abb280
George Thomas <147007612+Geot44@users.noreply.github.com>
2026-04-14 14:26:18 +0530

    Change module name in tb.v
    Updated module instantiation from tt_um_example to tt_um_processor_top.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example processor_top (
+  tt_um_processor_top user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
