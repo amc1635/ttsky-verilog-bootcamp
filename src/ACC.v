0b20b58686a68cf3d95d7f791bc23ab19f58c0aa
amc1635 <118891354+amc1635@users.noreply.github.com>
2026-04-14 13:23:06 +0530

    Update header comments in ACC.v
    Removed old header comments and added copyright notice.

--- src/ACC.v
+++ src/ACC.v
@@ -1,26 +1,9 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.03.2026 10:30:18
-// Design Name: 
-// Module Name: ACC
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-
-
+`default_nettype none
 module ACC(
     input clk,
     input reset,
