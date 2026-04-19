b5134cdb121acf770f9de151c73618caff82f523
amc1635 <118891354+amc1635@users.noreply.github.com>
2026-04-19 12:04:23 +0530

    Change instruction width from 8-bit to 12-bit
    Updated instruction widths in pipeline_register and instruction_register modules from 8 bits to 12 bits. Modified instruction_memory to handle 12-bit instructions and adjusted memory size.

--- src/WrapperIF.v
+++ src/WrapperIF.v
@@ -18,46 +18,90 @@ endmodule
 
 module pipeline_register(
 input clk,reset,
-input [7:0]instr_in,
-output reg[7:0] instr_out );
+input [11:0]instr_in,
+output reg[11:0] instr_out );
 always@(posedge clk)
 begin
 if(reset)
-instr_out<=8'b0;
+instr_out<=12'b0;
 else
 instr_out<=instr_in;
 end
 endmodule
 
-module instruction_register(
+
+
+module pc(
+input reset,clk,
+output reg [2:0] rd_addr);
+always@ (posedge clk)
+begin
+if(reset)
+rd_addr <=3'b0;
+else
+rd_addr<=rd_addr + 1;
+end
+endmodule
+    
+module instruction_register (
     input   clk,
     input   reset,
-    input  [7:0] Instruction,   
-    output reg  [7:0] instr_out);
+    input  [11:0] Instruction,   
+    output reg  [11:0] instr_out);
 
 always @(posedge clk ) begin
     if (reset)
-        instr_out <= 8'd0;
-    else
+        instr_out <= 12'd0;
+    else  
         instr_out <= Instruction;end
 endmodule
 
 
-module instruction_memory(
-    input  wire [7:0] PC_Address,   // Comes from Member 1's PC_out
-    output wire [7:0] Instruction   // Goes into Member 1's instr_in
+module instruction_memory (
+    input clk,
+    input rst,
+
+    // from SIPO
+    input [11:0] instr_in,
+    input done,              
+
+    // read side
+    input [2:0] rd_addr,
+    input re,
+
+    output reg [11:0] instr_out,
+    output reg [2:0] wr_addr
 );
 
-    // -------------------------------------------------------------------------
-    // The ROM Array
-    // 256 memory slots, each holding an 8-bit instruction.
-    // -------------------------------------------------------------------------
-    reg [7:0] rom [0:255];
+reg [11:0] mem [0:7];
+reg done_d;   // for edge detection
 
-    // Asynchronous Read
-    // The moment the PC changes, the new instruction instantly flows out.
-    // -------------------------------------------------------------------------
-    assign Instruction = rom[PC_Address];
+always @(posedge clk) begin
+    if (rst) begin
+        wr_addr <= 0;
+        instr_out <= 0;
+        done_d <= 0;
+    end else begin
+        done_d <= done;
+
+        
+        if (done && !done_d) begin
+            if (wr_addr < 3'd7) begin
+                mem[wr_addr] <= instr_in;
+                wr_addr <= wr_addr + 1;
+            end else begin
+                mem[wr_addr] <= instr_in; // last location
+                wr_addr <= wr_addr;       // stop increment
+            end
+        end
+
+        
+        if (re) begin
+            instr_out <= mem[rd_addr];
+        end
+    end
+end
+
 
 endmodule
 
