9351501e48a993d7c0b9cb4fad75c97e1a468548
amc1635 <118891354+amc1635@users.noreply.github.com>
2026-04-19 12:35:42 +0530

    Replace Decoder module with ex_decoder
    Refactor Decoder module to ex_decoder with updated input and output sizes.

--- src/Decoder.v
+++ src/Decoder.v
@@ -5,30 +5,14 @@
 
 `default_nettype none
 
-module Decoder(
-input [7:0]instr,
-output reg [2:0]alu_op,
-output reg acc_load,mem_write,
-output [7:0]operand_out);
-  wire [2:0]opcode;
-assign opcode  = instr[7:5];
-assign operand_out = {3'b000,instr[4:0]};
-parameter[2:0]LOAD = 3'b110, STORE= 3'b111;
-always@(*) begin
-if(opcode==STORE)begin
-alu_op = STORE;
-acc_load = 0;
-mem_write = 1;
-end
-else if(opcode==LOAD)begin
-alu_op   = LOAD;
-acc_load = 1;
-mem_write = 0;
-end
-else begin
-alu_op   = opcode;
-acc_load = 1;
-mem_write = 0;
-end
-end
+module ex_decoder (
+    input  wire [11:0] instr_in,
+    output wire [3:0]  opcode,
+    output wire [7:0]  operand
+);
+
+    // Physically split the incoming bus
+    assign opcode  = instr_in[11:8];
+    assign operand = instr_in[7:0];
+
 endmodule
